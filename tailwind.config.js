71638001094ec0479f72cf69c5cc690c896d4850
gabste8831 <gabrielsteffens2003@gmail.com>
2025-06-22 18:53:05 -0300

    Versão com o front ajustado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
-  theme: {
-    extend: {
-      colors: {
-        'mechanic-gray': {
-          50: '#f9fafb',
-          100: '#f3f4f6',
-          200: '#e5e7eb',
-          300: '#d1d5db',
-          400: '#9ca3af',
-          500: '#6b7280',
-          600: '#4b5563',
-          700: '#374151',
-          800: '#1f2937',
-          900: '#111827',
-        }
-      }
-    },
-  },
+  theme: {},
   plugins: [],
-} 
+}; 
